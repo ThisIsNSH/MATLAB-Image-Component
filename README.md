1725105dcb92cc3458a5befcffdcca715d5c3157
ThisIsNsh <hadanis.singh@gmail.com>
2020-01-05 15:52:35 +0530

    Files Added

---
+++ README.md
@@ -0,0 +1,12 @@
+# MATLAB-Image-Component
+
+### Image Processing
+
+Finding 4-Way connected components in a BW Image.
+--------
+* I have taken a matrix `a` which in similar to 2-D pixel array of an BW image containing 1 & 0.
+* DFS technique is used to count all the components.
+
+<img src="im1.PNG" width="25%">  
+
+
---
+++ comp.m
@@ -0,0 +1,23 @@
+function a = comp(arr) 
+    count=0;
+    for row = 1:size(arr)(1)
+      for col = 1:size(arr)(2)
+        if arr(row,col) == 1
+          arr = dfs(row,col,arr);
+          count=count+1;
+        endif
+      endfor
+    endfor
+    a=count;
+end
+
+function b = dfs(row,col,arr)
+  if row>=1 && row<=size(arr)(1) && col>=1 && col<=size(arr)(2) && arr(row,col)==1
+    arr(row,col) = 0;
+    arr = dfs(row+1,col,arr);
+    arr = dfs(row-1,col,arr);
+    arr = dfs(row,col+1,arr);
+    arr = dfs(row,col-1,arr);
+  endif
+  b=arr;
+endfunction
